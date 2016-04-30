4bb23c86b49a43729aa7aacd46754188490a664d
renye_edison <renye@ntu.edu.sg>
2016-04-30 09:11:55 +0000

    change to class

--- csc_test.py
+++ csc_test.py
@@ -19,6 +19,84 @@
 import pexpect
 import sys
 import time
+from heartrate_test import HeartRate
+
+class CSC(HeartRate):
+    """ Other functions inherited from parent """
+    def __init__(self, bluetooth_adr, perimeter=2055, debug=False):
+        sup(CSC, self).__init__(bluetooth_adr, debug)
+        self.perimeter = perimeter
+        self.current_data = self.past_data = \
+               {"wheel_rev": 0,\
+                "last_wheel_event": 0,\
+                "crank_rev": 0,\
+                "last_crank_event": 0}
+
+    def delta(self, current, past, bits=16):
+        Delta = current - past
+        if Delta < 0:
+            Delta += 2**bits
+        return Delta
+
+    def cal_speed(self):
+        """ wheel rev, wheel last event time,
+        speed m/s is perimeter (m) *revolution / delta T (s)
+        delta T wraps at 2**16-1: 65535
+        wheel_rev wraps at 2**32-1
+        """
+        current_rev, past_rev = self.current_data["wheel_rev"], \
+                self.past_data["wheel_rev"]
+        current_time, past_time = self.current_data["last_wheel_event"], \
+                self.past_data["last_wheel_event"]
+        delta_w_rev = self.delta(current_rev, past_rev, 32)
+        delta_w_t = self.delta(current_time, past_time, 16)
+
+        return self.perimeter * \
+                delta_w_rev / delta_w_t * 3.6 * 1.024# (km/h)
+        # perimeter mm need /1000 and delta t ms need /1024**-1,
+        # finally they are canceled by a factor of 1.024
+
+    def cal_cadence(self):
+        """crank rev, crank last event time,
+        cadence r/m is revolutin / delta T (min),
+        delta T wraps at 2**16-1: 65535
+        crank_rev wraps at 2**16-1
+        """
+        current_rev, past_rev = self.current_data["crank_rev"], \
+                self.past_data["crank_rev"]
+        current_time, past_time = self.current_data["last_crank_event"], \
+                self.past_data["last_crank_event"]
+        delta_c_rev = self.delta(current_rev, past_rev, 16)
+        delta_c_t = self.delta(current_time, past_time, 16)
+
+        return delta_c_rev / \
+                (delta_c_t / 1024.0 / 60)  #(rpm)
+
+    def parse_data(self):
+        wheel_rev = int(reduce(lambda x,y:x+y, self.rval[9:5:-1]), 16)
+        last_wheel_event = int(reduce(lambda x,y:x+y, self.rval[11:9:-1]), 16)
+        crank_rev = int(reduce(lambda x,y:x+y, self.rval[13:11:-1]), 16)
+        last_crank_event = int(reduce(lambda x,y:x+y, self.rval[15:13:-1]), 16)
+        return {"wheel_rev": wheel_rev,\
+                "last_wheel_event": last_wheel_event,\
+                "crank_rev": crank_rev,\
+                "last_crank_event": last_crank_event}
+        
+    def retrieve_data(self):
+        self.past_data = self.current_data
+        try:
+            pnum = self.con.expect('Notification handle = .*')
+        except pexpect.TIMEOUT:
+            print "TIMEOUT exception"
+            break
+        if pnum == 0: # no error
+            self.rval = self.con.after.split()
+            self.current_data = parse_data(self)
+            self.speed, self.cadence = \
+                    self.cal_speed, self.cal_cadence
+        else:
+            return -1
+
 
 bluetooth_adr = sys.argv[1]
 tool = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' -I -t random')
--- heartrate_test.py
+++ heartrate_test.py
@@ -20,24 +20,92 @@ import pexpect
 import sys
 import time
 
-bluetooth_adr = sys.argv[1]
-tool = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' -I -t random')
-tool.expect('\[LE\]>')
-print "Preparing to connect. You might need to press the side button..."
-tool.sendline('connect')
-# test for success of connect
-tool.expect('Connection successful')
-print "connect successful"
-tool.sendline('char-read-uuid 0x2902')
-tool.expect('0x0018')
-print "found 0x0018"
-tool.sendline('char-write-req 0x0018 0100')
-# tool.expect('successfully')
-# print "successfully"
-while True:
-    tool.expect('Notification handle = .*')
+class HeartRate(object):
+    """connect heartrate BLE device to edison and read heart rate """
 
-    rval = tool.after.split()
-    print int(rval[5] + rval[6], 16)
+    def __init__(self, bluetooth_adr, debug=False):
+
+        # connect to gatttool, -I (--interactives) -t random
+        self.con = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' -I -t random')
+        self.debug = debug
+
+        # expect LE: low energy
+        self.con.expect('\[LE\]>')
+        if self.debug:
+            print "Preparing to connect. You might need to press the side button..."
+
+    def char_write_cmd(self, handle, value):
+        cmd = 'char-write-cmd 0x%04x 0%x' % (handle, value)
+        if self.debug:
+            print cmd
+        self.con.sendline(cmd)
+
+    def char_read_uuid(self, uuid, expected):
+        cmd = 'char-read-uuid 0x%04x' % uuid
+        if self.debug:
+            print cmd
+        self.con.sendline(cmd)
+        if type(expected) is str:
+            self.con.expect(expected)
+        elif type(expected) is int:
+            self.con.expect("0x%04x" % expected)
+
+    def print_heartrate(self):
+        if self.rval is not None:
+            return int(reduce(lambda x,y:x+y, rval[5:7]), 16)
+
+    def connect(self):
+        # connect device
+        self.con.sendline('connect')
+        # test for success of connect
+        self.con.expect('Connection successful')
+        if self.debug:
+            print "connect successful"
+
+    def subscribe(self):
+        # write 0100 to handle to enable notification
+        self.char_write_cmd(0x0018, 0100)
+        exit_flag = self.con.expect('successfully')
+
+        if exit_flag == 0: # no error
+            return 0
+    
+    def unsubscribe(self):
+        self.char_write_cmd(0x0018, 0000)
+
+    def disconnect(self):
+        self.con.sendline('disconnect')
+        self.con.sendline('exit')
+        if self.con.isalive():
+                self.con.sendline('exit')
+
+        # wait one second for gatttool to stop
+        for i in range(100):
+            if not self.con.isalive(): break
+            time.sleep(0.01) 
+            
+        self.con.close()
+
+    def __str__(self):
+        try:
+            pnum = self.con.expect('Notification handle = .*')
+        except pexpect.TIMEOUT:
+            print "TIMEOUT exception"
+            break
+        if pnum == 0: # no error
+            self.rval = self.con.after.split()
+            return print_heartrate(self)
+        else:
+            return -1
 
 
+if __name__ == "__main__":
+    HRM = HeartRate(sys.argv[1], debug=True) 
+    HRM.connect()
+    HRM.subscribe()
+    try:
+        while True:
+            print HRM
+    except KeyboardInterrupt:
+        HRM.unsubscribe()
+        HRM.disconnect()
