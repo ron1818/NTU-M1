cde89b0c1ad563d12ed77e03c34b5206a40f8c09
renye_edison2 <renye@ntu.edu.sg>
2016-06-18 08:31:53 +0000

    display bike

--- display_bike.py
+++ display_bike.py
@@ -9,20 +9,26 @@ def write_thingspeak(field_id, field_var):
     channel = thingspeak.channel()
     channel.update(field_id, field_var)
 
-
 # connection
 OLED = Digole("/dev/ttyMFD1", width=160, height=128)
+# global speed, distance, t0, t1
+t0 = time.time()
+t1 = time.time()
+distance = 0
+speed = 0.0
 
 # draw framework
 OLED.clearScreen()
-draw_in_quardrant(OLED, draw_gui_1, 3)
-# draw_gui_1(OLED)
+draw_in_quardrant(OLED, display_perform, 3, 0.5, 18)
+draw_in_quardrant(OLED, display_alert, 4, 0.5, 0)
 
 # connect BLE
-HRM = HeartRate(sys.argv[1], debug=True) 
+HRM = HeartRate("D0:F1:73:6F:94:D9", debug=True) 
+# HRM = HeartRate("E9:E3:B8:EC:D4:64", debug=True) 
 # HRM.main()
 
-CSC = CSC(sys.argv[2], debug=True)
+CSC = CSC("E9:E3:B8:EC:D4:64", debug=True)
+# CSC = CSC(sys.argv[2], debug=True)
 # CSC.main()
 HRM.connect()
 HRM.subscribe()
@@ -36,24 +42,30 @@ try:
     OLED.flush()
     counter = 0
     while True:
+        # empty display data
         data_dict={"hrm": ["   ", 105, 52],\
                 "speed": ["    ", 62, 82],\
-                "cadence": [25, 52]}
+                "cadence": ["   ", 25, 52]}
 
-        display_data(OLED, **data_dict)
-        # OLED.drawStr(105, 52, "   ")
-        # OLED.drawStr(62, 82, "    ")
-        # OLED.drawStr(25, 52, "   ")
+        draw_in_quardrant(OLED, display_trip, 2, 0.5, 18)
+        draw_in_quardrant(OLED, display_timedate, 1, 0.5, 18)
+        draw_in_quardrant(OLED, display_data, 3, 0.5, 18, **data_dict)
+
         data_dict["hrm"][0] = HRM.__str__().strip()
         data_dict["speed"][0], data_dict["cadence"][0] = CSC.__str__().strip().split(",")
+
+        speed = data_dict["speed"][0]
+
         # print ", ".join([hrm, speed, cadence])
-        display_data(OLED, **data_dict)
+        draw_in_quardrant(OLED, display_data, 3, 0.5, 18, **data_dict)
         
         if counter == 60:
-            write_thingspeak([1, 2, 3], [value[0] for (key, value) in a.iteritems()])
+            write_thingspeak([1, 2, 3], [value[0] for (key, value) in data_dict.iteritems()])
             counter = 0
         counter += 1
         time.sleep(1)
+    OLED.setColor(0xFF)
+    OLED.write_command("BGC", 0x00)
 except KeyboardInterrupt:
     OLED.flush()
     HRM.unsubscribe()
--- draw_GUI.py
+++ draw_GUI.py
@@ -1,7 +1,7 @@
 from Digole_OLED_serial import *
 import time
 
-def scale_font(scale, font):
+def scale_font(OLED, scale, font):
     # set font
     scaled_font = int(font*scale)
     if 0 <= scaled_font < 8:
@@ -21,7 +21,7 @@ def display_perform(OLED, scale=1, font=18):
     top ones are for RPM and BPM,
     bottom one is for Km/h"""
     # scale font
-    scale_font(scale, font)
+    scale_font(OLED, scale, font)
 
     # set mode
     OLED.setMode("C")
@@ -59,7 +59,7 @@ def display_trip(OLED, scale=1, font=18, stopflag=False):
     """ display trip time and trip distance,
     including autostop """
 
-    scale_font(scale, font)
+    scale_font(OLED, scale, font)
     # set mode
     OLED.setMode("C")
 
@@ -68,17 +68,16 @@ def display_trip(OLED, scale=1, font=18, stopflag=False):
     OLED.write_command("BGC", 0x00)
 
     # draw upper rectangle
-    OLED.setColor(0xDD)
+    OLED.setColor(0xF8)
     # draw upper rectangle
-    OLED.fillRoundRect(int(0*scale), int(0*scale), int(80*scale), int(20*scale))
+    OLED.fillRoundRect(int(1*scale), int(1*scale), int(100*scale), int(20*scale))
     # set background color
-    OLED.write_command("BGC", 0xDD)
+    OLED.write_command("BGC", 0xF8)
     # font color
     OLED.setColor(0x1c)
 
     # time elapsed
-    global t0
-    global t1
+    global t0, t1, distance, speed
     t_old = t1
     if stopflag == False:
         t1 = time.time()
@@ -86,16 +85,15 @@ def display_trip(OLED, scale=1, font=18, stopflag=False):
     m, s = divmod(t1-t0, 60)
     h, m = divmod(m, 60)
     # print "%d:%02d:%02d" % (h, m, s)
-    OLED.drawStr(int(0*scale), int(18*scale), "%02d:%02d:%02d" % (h, m, s))
+    OLED.drawStr(int(1*scale), int(19*scale), "%02d:%02d:%02d" % (h, m, s))
     
     # draw lower rectangle
     OLED.setColor(0x6B)
-    OLED.fillRoundRect(int(0*scale), int(22*scale), int(80*scale), int(42*scale))
+    OLED.fillRoundRect(int(1*scale), int(23*scale), int(100*scale), int(42*scale))
     OLED.write_command("BGC", 0x6B)
     # font color
     OLED.setColor(0x1c)
     
-    global distance
 
     # trip accumulated
     if stopflag == False:
@@ -108,26 +106,44 @@ def display_trip(OLED, scale=1, font=18, stopflag=False):
     OLED.write_command("BGC", 0x00)
 
 
-def display_data(OLED, scale=1,  **data_dict):
+def display_data(OLED, scale=1, font=18, **data_dict):
     """ display data into their positions,
     data_dict: key: data_name, [value, x, y] """
+    # scale font
+    scale_font(OLED, scale, font)
+
     for key, value in data_dict.iteritems():
         data, x, y = value
-        OLED.drawStr(data, int(x*scale), int(y*scale))
+        OLED.drawStr(int(x*scale), int(y*scale), data)
 
 
 def display_timedate(OLED, scale=1, font=18):
     """ display time and date, also time elapsed """
     # scale font
-    scale_font(scale, font)
+    scale_font(OLED, scale, font)
 
     # time in wk dd-mm-yy format
     OLED.drawStr(int(1*scale), int(18*scale), time.strftime("%a %d-%m-%y"))
     OLED.drawStr(int(1*scale), int(36*scale), time.strftime("%H:%M:%S"))
 
 
+def display_alert(OLED, scale=1, isfall=0):
+    """ display fall signal, green for OK, red for fall """
+    if isfall:
+        OLED.setColor(0x40)
+        OLED.write_command("BGC", 0x40)
+    else:
+        OLED.setColor(0x1D)
+        OLED.write_command("BGC", 0x1D)
 
-def draw_in_quardrant(OLED, draw_fn, quardrant=2):
+    OLED.drawCircle(int(80*scale), int(44*scale), int(40*scale),1)
+
+    # set background to black
+    OLED.setColor(0xFF)
+    OLED.write_command("BGC", 0x00)
+
+
+def draw_in_quardrant(OLED, draw_fn, quardrant=2, *args, **kwargs):
     """ draw a particular function at the 1/4 quardrant,
     follow cartesian (1,2,3,4) CCW,
     notice that for the child draw fn, scale must be made"""
@@ -141,7 +157,7 @@ def draw_in_quardrant(OLED, draw_fn, quardrant=2):
     else:
         OLED.setDrawWindow(80, 64, 80, 64)
         
-    draw_fn(OLED, scale=0.5)
+    draw_fn(OLED, *args, **kwargs)
 
 if __name__ == "__main__":
     # connection
@@ -151,8 +167,16 @@ if __name__ == "__main__":
     t1 = time.time()
     distance = 0
     speed = 5.0
-    while True:
-        draw_in_quardrant(OLED, display_perform, 3)
-        draw_in_quardrant(OLED, display_trip, 2)
-        draw_in_quardrant(OLED, display_timedate, 1)
-        time.sleep(1)
+    draw_in_quardrant(OLED, display_perform, 3, 0.5, 18)
+    # draw_in_quardrant(OLED, display_alert, OLED, 4, 0.5, 0)
+    # data_dict={"hrm": ["100", 105, 52],\
+    #             "speed": ["100", 62, 82],\
+    #             "cadence": ["60", 25, 52]}
+    # # display_perform(OLED)
+    # # display_data(OLED,**data_dict)
+    # while True:
+    #     draw_in_quardrant(OLED, display_trip, OLED, 2, 0.5, 18)
+    #     draw_in_quardrant(OLED, display_timedate, OLED, 1, 0.5, 18)
+    #     draw_in_quardrant(OLED, display_data, OLED, 3, 0.5, 18, **data_dict)
+    #     # draw_in_quardrant(OLED, display_alert, 4, 0.5, 1)
+    #     time.sleep(1)
